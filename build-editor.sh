7e547cf3eea86d5968ac51dd8b28f4629b9dd9af
Tim Yuen <timothy.yuen22@gmail.com>
2022-08-28 21:24:12 -0400

    update universal bin name for mac

--- build-editor.sh
+++ build-editor.sh
@@ -5,7 +5,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     scons platform=osx arch=arm64 target=release_debug tools=yes -j8
     scons platform=osx arch=x86_64 target=release_debug tools=yes -j8
-    lipo -create bin/godot.osx.opt.tools.arm64 bin/godot.osx.opt.tools.x86_64 -output bin/godot.osx.opt.universal
+    lipo -create bin/godot.osx.opt.tools.arm64 bin/godot.osx.opt.tools.x86_64 -output bin/godot.osx.opt.tools.universal
 else
     scons platform=windows target=release_debug tools=yes -j8
 fi
