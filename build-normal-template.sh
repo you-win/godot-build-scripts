3d213cd5b301e940b10b24f2b10609ad554293d2
Tim Yuen <timothy.yuen22@gmail.com>
2022-09-01 15:35:49 -0400

    fix bad param for linux release builds

--- build-normal-template.sh
+++ build-normal-template.sh
@@ -3,7 +3,7 @@
 set -e
 
 if  [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    scons platform=windows target=release tools=no use_lto=yes module_gtlf=yes -j8
+    scons platform=x11 target=release tools=no use_lto=yes module_gtlf=yes -j8
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     scons platform=osx arch=arm64 target=release tools=no module_gltf=yes -j8
     scons platform=osx arch=x86_64 target=release tools=no module_gltf=yes -j8
